192527f692f8660e9cc65be0225e7e2a811b418f
Eoln Msuk <90252497+EolnMsuk@users.noreply.github.com>
2025-02-21 05:39:27 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 # config.py
 
 # All your bot's configuration and constants go here
-GUILD_NAME = "OmegleStream (18+)"
+GUILD_NAME = "YourGuildNameHere"  # Replace with your guild name
 
 # Replace channel names with their IDs
-COMMAND_CHANNEL_ID = 1334294218826453072  # Replace with the actual ID of the command channel
-CHAT_CHANNEL_ID = 1207213216849985596     # Replace with the actual ID of the chat channel
-STREAMING_VC_ID = 1131770315740024966     # Replace with the actual ID of the Streaming VC
-HELL_VC_ID = 1134660138955980820          # Replace with the actual ID of the Hell VC
+COMMAND_CHANNEL_ID = 123456789012345678  # Replace with the actual ID of the command channel
+CHAT_CHANNEL_ID = 123456789012345678     # Replace with the actual ID of the chat channel
+STREAMING_VC_ID = 123456789012345678     # Replace with the actual ID of the Streaming VC
+HELL_VC_ID = 123456789012345678          # Replace with the actual ID of the Hell VC
 
 ALLOWED_USERS = {
-    "elonmusk4334", 
-    "elonmuskalt4334", 
-    "Jockie Music#8158", 
-    "Omegle Bot#0081"
+    "User1", 
+    "User2", 
+    "User3#1234", 
+    "BotName#1234"
 }
 COMMAND_COOLDOWN = 5  # Seconds
 HELP_COOLDOWN = 5     # Seconds
@@ -27,12 +27,18 @@ SOUND_FILE = "skip.mp3"  # Path to your sound file
 # URL of the Uhmegle site (video page)
 UHMEGLE_VIDEO_URL = "https://uhmegle.com/video"
 
+# Welcome message (for on_member_join)
+WELCOME_MESSAGE = (
+    "Welcome to the Discord {mention}! 🎉\n"
+    "Go to https://discord.com/channels/123456789012345678/123456789012345678 then type !help"
+)
+
 # Rules message
 RULES_MESSAGE = (
     "Welcome to the **Streaming  📷** VC!\n\n"
     "**Rules:**\n"
     "1. Please have your camera on while in the Streaming VC. If your camera is off, you will be moved out, repeated violations may result in longer timeouts.\n"
-    "2. Control skipping and more for the Omegle Stream Bot https://discord.com/channels/1131770315740024962/1334294218826453072 GC.\n"
+    "2. Control skipping and more for the Omegle Stream Bot https://discord.com/channels/123456789012345678/123456789012345678 GC.\n"
     "3. Please be sure to !stop the bot when no one is using it (prevents ban).\n\n"
     "Thank you for your cooperation!"
 )
